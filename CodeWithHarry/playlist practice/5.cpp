ad717704d997f2d346150b27d7f897e0f80560f7
Dipesh Kharel <mr.dipesh333@gmail.com>
2026-07-07 13:20:24 +0545

    Done upto 7

--- CodeWithHarry/playlist practice/5.cpp
+++ CodeWithHarry/playlist practice/5.cpp
@@ -41,3 +41,32 @@ int main(){
 
     return 0;
 }
+
+
+
+
+
+/*
+
+===================
+IMPORTANT POINTS
+===================
+1. The sign "<<" is called insertion operator 
+2. The sign ">>" is called extraction operator 
+3. "cout" keyword is used to print 
+4. "cin" keyword is used to take input at run time
+
+*/
+
+
+
+
+
+/*
+
+==========================
+Reserved Keywords in C++
+==========================
+Reserved Keywords are those keywords that are used by the language itself, which is why these keywords are not available for redefinition or overloading. In short, you cannot create variables with these names. 
+
+*/
